882373b839aea344e731fc8f0ff28de6e8dd61d0
Alan Mimms <alanmimms@gmail.com>
2017-09-21 14:16:47 -0700

    Use float version, add SJSON and a few others since we have room

--- tools/alan-flash.sh
+++ tools/alan-flash.sh
@@ -1,5 +1,5 @@
-FILE=$( ls -1 -t bin/nodemcu_integer_*.bin | head -n 1 )
+FILE=$( ls -1 -t bin/nodemcu_float_*.bin | head -n 1 )
 echo
 echo "------ Flashing $FILE ------"
 echo
-esptool.py -p /dev/ttyUSB0 write_flash 0 $FILE  0x3fc000 ../ESP8266_NONOS_SDK-v2.0.0/bin/esp_init_data_default.bin 0x3fe000 ../ESP8266_NONOS_SDK-v2.0.0/bin/blank.bin 
+~/.local/bin/esptool.py -p /dev/ttyUSB0 write_flash 0 $FILE  0x3fc000 ../ESP8266_NONOS_SDK-v2.0.0/bin/esp_init_data_default.bin 0x3fe000 ../ESP8266_NONOS_SDK-v2.0.0/bin/blank.bin 
